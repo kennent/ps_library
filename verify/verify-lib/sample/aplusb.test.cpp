3dabe08893e0d344656de37d0338bccf0ea1e6d1
kennen <kckyoung11@gmail.com>
2025-05-10 02:16:02 +0900

    test verify

--- verify/verify-lib/sample/aplusb.test.cpp
+++ verify/verify-lib/sample/aplusb.test.cpp
@@ -1,12 +1,11 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 
-#include "./template/template.hpp"
-#include "./template/fast-IO.hpp"
+#include "../../../template/template.hpp"
+#include "../../../template/fast-IO.hpp"
 
 using namespace kENN3N;
 
-solve() {
-  fastIO();
+void kENN3N::solve() {
   int a, b;
   cin >> a >> b;
   cout << a+b << "\n";
